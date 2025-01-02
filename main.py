d6f1a0e1d6ad3003b15acab7a1debd42bd334033
Dervillay <james.gascoigneburns7@gmail.com>
2025-01-02 12:30:56 +0000

    Preserve context to enable conversations

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sentence_transformers import SentenceTransformer
 from tinydb import TinyDB, where
 from config import (
     DETERMINE_BOARD_GAME_PROMPT_TEMPLATE,
+    SYSTEM_PROMPT,
     EXPLAIN_RULES_PROMPT_TEMPLATE,
     EMBEDDING_MODEL_PATH,
     DATABASE_PATH,
@@ -31,15 +32,15 @@ class BoardBrain:
         embedding_database_path: str,    
         normalize_embeddings: bool,
     ):
-        self.selected_board_game = None
-        self.known_board_games = [board_game["name"] for board_game in BOARD_GAMES]
+        self.selected_board_game: str = None
+        self.known_board_games: List[str] = [board_game["name"] for board_game in BOARD_GAMES]
+        self.message_history = {board_game["name"]: [] for board_game in BOARD_GAMES}
         self.__openai_model_to_use = open_ai_model_to_use
         self.__normalize_embeddings = normalize_embeddings
         self.__embedding_db = TinyDB(embedding_database_path)
         self.__rulebook_pages = self.__embedding_db.table("rulebook_pages")
         self.__embedding_model = SentenceTransformer(embedding_model_path)
         self.__openai_client = openai.OpenAI()
-        self.messages = []
 
     def __construct_messages(
         self,
@@ -91,6 +92,39 @@ class BoardBrain:
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
 
+    def __get_relevant_rulebook_extracts(
+        self,
+        question: str,
+        n: int = 5,
+    ):
+        question_embedding = self.__embed_question(question)
+        pages = self.__rulebook_pages.search(
+            where("board_game_name") == self.selected_board_game
+        )
+
+        results = []
+        for page in pages:
+            for chunk in page["chunks"].values():
+                cosine_similarity = np.dot(
+                    np.array(question_embedding),
+                    np.array(chunk["embedding"]),
+                )
+                results.append(
+                    {
+                        "rulebook_name": page["rulebook_name"],
+                        "page_num": page["page_num"],
+                        "text": chunk["text"],
+                        "similarity": cosine_similarity,
+                    }
+                )
+        results = sorted(
+            results,
+            key=lambda x: x["similarity"],
+            reverse=True,
+        )
+
+        return results[:n]
+
     def __construct_rulebook_link(
         self,
         citation: dict,
@@ -140,55 +174,37 @@ class BoardBrain:
 
         return self.selected_board_game
     
-    def get_relevant_rulebook_extracts(
-        self,
-        board_game_name: str,
-        question: str,
-        n: int = 5,
-    ):
-        question_embedding = self.__embed_question(question)
-        pages = self.__rulebook_pages.search(
-            where("board_game_name") == board_game_name
-        )
-
-        results = []
-        for page in pages:
-            for chunk_id, chunk in page["chunks"].items():
-                cosine_similarity = np.dot(
-                    np.array(question_embedding),
-                    np.array(chunk["embedding"]),
-                )
-                results.append(
-                    {
-                        "rulebook_name": page["rulebook_name"],
-                        "page_num": page["page_num"],
-                        "chunk_id": chunk_id,
-                        "text": chunk["text"],
-                        "similarity": cosine_similarity,
-                    }
-                )
-        results = sorted(
-            results,
-            key=lambda x: x["similarity"],
-            reverse=True,
-        )
-
-        return results[:n]
-    
-    def explain_rules(
+    def ask_question(
         self,
         question: str,
-        rulebook_extracts: List
     ):
+        # Get N most relevant chunks of rulebook text for the selected board game
+        rulebook_extracts = self.__get_relevant_rulebook_extracts(question)
         rulebook_extracts_as_string = "\n".join([json.dumps(extract) for extract in rulebook_extracts])
-        messages = self.__construct_messages(
+
+        prompt = (
             EXPLAIN_RULES_PROMPT_TEMPLATE
-            .replace("<BOARD_GAME>", self.selected_board_game)
+            .replace("<SELECTED_BOARD_GAME>", self.selected_board_game)
             .replace("<RULEBOOK_EXTRACTS>", rulebook_extracts_as_string)
             .replace("<QUESTION>", question)
         )
-        response = self.__call_openai_model(messages)
-        return self.__parse_citations(response)
+
+        # If this is the first question about this board game, include the system prompt
+        # otherwise, pass previous messages to the model so it retains context
+        previous_messages_for_selected_board_game = self.message_history[self.selected_board_game]
+        if len(previous_messages_for_selected_board_game) == 0:
+            messages = self.__construct_messages(SYSTEM_PROMPT + prompt)
+        else:
+            messages = self.__construct_messages(prompt, previous_messages_for_selected_board_game)
+
+        response_with_metadata = self.__call_openai_model(messages, return_all_metadata=True)
+
+        # Parse the response and store it in message history
+        response_message = response_with_metadata.choices[0].message
+        response_message.content = self.__parse_citations(response_message.content)
+        self.message_history[self.selected_board_game].append(response_message)
+
+        return response_message.content
 
 
 def main():
@@ -199,12 +215,16 @@ def main():
         NORMALIZE_EMBEDDINGS,
     )
 
-    question = input(">")
-    selected_board_game = board_brain.determine_board_game(question)
-    rulebook_extracts = board_brain.get_relevant_rulebook_extracts(selected_board_game, question)
-    response = board_brain.explain_rules(question, rulebook_extracts)
-    print(f"\n\nSelected board game: {selected_board_game}")
-    print(f"Response: {response}")
+    while True:
+        question = input(">")
+
+        if board_brain.selected_board_game == None:
+            board_brain.determine_board_game(question)
+        
+        response = board_brain.ask_question(question)
+
+        print(f"\n\nSelected board game: {board_brain.selected_board_game}")
+        print(f"Response: {response}")
 
 
 if __name__ == "__main__":
