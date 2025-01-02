993f801406902f881bcb126899faa3e6f94a9a39
Dervillay <james.gascoigneburns7@gmail.com>
2025-01-02 10:26:12 +0000

    Add more consts

--- config.py
+++ config.py
@@ -40,6 +40,11 @@ BOARD_GAMES = [
 ]
 BOARD_GAMES_STRING_LIST = "\n".join([f"- {board_game['name']}" for board_game in BOARD_GAMES])
 
+UNKNOWN_BOARD_GAME_RESPONSE = """
+I'm unable to determine which board game your question refers to.
+Please select one manually from the dropdown or try asking me another question.
+"""
+
 DETERMINE_BOARD_GAME_PROMPT_TEMPLATE = f"""
 You will be given a list of board games and a question, where you must determine which board game in the list the question is about.
 You must answer with the name of the board game exactly as it appears in the list.
@@ -47,6 +52,27 @@ If the question is about a board game that isn't in the list, or the question is
 
 The list is:
 {BOARD_GAMES_STRING_LIST}
+
+The question is:
+<QUESTION>
+"""
+
+EXPLAIN_RULES_PROMPT_TEMPLATE = f"""
+You are an intellectually honest assistant that helps users understand the rules for different board games.
+You will be given several JSON objects containing text extracts from rulebooks for a board game. You will also be given a question about the rules of this board game.
+You must answer this question using only the information contained in the 'text' field of these JSON objects.
+Only consider an extract's text value if it is directly relevant to the question asked.
+You may quote relevant rulebook text in your response, but must cite its source using the following format: {{"rulebook_name":<rulebook_name>, "page_num":<page_num>}}.
+
+If the rulebook extracts are not sufficient for you to confidently answer the question: tell the user that you couldn't find this information in any of the rulebooks.
+If you aren't certain of the answer but think you have a reasonable interpretation of the rules: give your interpretation, but step through your reasoning and make it clear that
+this is only an interpretation.
+
+The board game is:
+<SELECTED_BOARD_GAME>
+
+The rulebook extracts are:
+<RULEBOOK_EXTRACTS>
 
 The question is:
 <QUESTION>
