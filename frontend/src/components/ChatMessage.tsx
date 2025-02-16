1751a281b7cdb7dbad33f4c9449c79e40b41feca
Dervillay <james.gascoigneburns7@gmail.com>
2025-02-16 13:46:16 +0000

    Refactor to use role over boolean

--- frontend/src/components/ChatMessage.tsx
+++ frontend/src/components/ChatMessage.tsx
@@ -3,23 +3,23 @@ import ReactMarkdown from "react-markdown";
 import { Box, Container, Flex } from "@chakra-ui/react";
 
 interface ChatMessageProps {
-	message: string;
-	isUser: boolean;
+	content: string;
+	role: "user" | "assistant";
 }
 
-export const ChatMessage: React.FC<ChatMessageProps> = ({ message, isUser }) => {
+export const ChatMessage: React.FC<ChatMessageProps> = ({ content, role }) => {
 	return (
 		<Container maxW="48rem">
-			<Flex justifyContent={isUser ? "flex-end" : "flex-start"}>
+			<Flex justifyContent={role === "user" ? "flex-end" : "flex-start"}>
 				<Box
-					maxW={isUser ? "70%" : "100%"}
-					bg={isUser ? "gray.800" : "gray.100"}
-					color={isUser ? "white" : "black"}
+					maxW={role === "user" ? "70%" : "100%"}
+					bg={role === "user" ? "gray.800" : "gray.100"}
+					color={role === "user" ? "white" : "black"}
 					px={5}
 					py={2.5}
 					borderRadius="1.5rem"
 				>
-					<ReactMarkdown>{message}</ReactMarkdown>
+					<ReactMarkdown>{content}</ReactMarkdown>
 				</Box>
 			</Flex>
 		</Container>
