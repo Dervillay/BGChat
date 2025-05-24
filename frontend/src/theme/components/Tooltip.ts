121ca598761e5f7cbc9e62c475e106b81691ea1e
Dervillay <james.gascoigneburns7@gmail.com>
2025-05-24 11:56:52 +0100

    Add common tooltip styling

---
+++ frontend/src/theme/components/Tooltip.ts
@@ -0,0 +1,10 @@
+export const Tooltip = {
+    baseStyle: {
+        bg: "gray.900",
+        color: "white",
+        borderRadius: "10px",
+        fontSize: "xs",
+        py: 1,
+        px: 2,
+    },
+};
--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -3,6 +3,7 @@ import { Input } from './components/Input.ts';
 import { Button } from './components/Button.ts';
 import { Select } from './components/Select.ts';
 import { Link } from './components/Link.ts';
+import { Tooltip } from './components/Tooltip.ts';
 import { gradients } from './gradients.ts';
 
 export const theme = extendTheme({
@@ -12,5 +13,6 @@ export const theme = extendTheme({
         Button,
         Select,
         Link,
+        Tooltip,
     },
 }); 
