51fcbcb209f3f490c586d1bf5ef3ced33512486d
Dervillay <james.gascoigneburns7@gmail.com>
2025-05-24 11:34:59 +0100

    Minor style tweaks

--- frontend/src/components/LoadingScreen.tsx
+++ frontend/src/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import { Box, Center, Spinner } from "@chakra-ui/react";
 export const LoadingScreen = () => {
 	return (
     <Center h="100vh">
-			<Spinner size="xl" color="gray.200" thickness="4px" speed="0.65s" />
+			<Spinner size="xl" color="gray.300" speed="0.65s" />
 		</Center>
 	);
 }; 
