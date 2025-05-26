e062c5d55bb8a648c9c9bfdb451a6d3cca5f5f1f
Dervillay <james.gascoigneburns7@gmail.com>
2025-05-26 22:52:50 +0100

    Remove database path

--- backend/app/config/paths.py
+++ backend/app/config/paths.py
@@ -5,4 +5,3 @@ backend_path = os.path.abspath(os.path.join(__file__, "../../../"))
 
 RULEBOOKS_PATH = os.path.join(backend_path, "resources/rulebooks")
 EMBEDDING_MODEL_PATH = os.path.join(backend_path, "resources/embedding_model")
-DATABASE_PATH = os.path.join(backend_path, "resources/database.json") 
