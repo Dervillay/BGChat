e9a583ac9e3192c40d114e7e5cb331ef3355c242
Dervillay <james.gascoigneburns7@gmail.com>
2025-07-14 21:08:23 +0100

    Remove unnecessary files

--- frontend/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders BGChat title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/BGChat/i);
-  expect(titleElement).toBeInTheDocument();
-});
