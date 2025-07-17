80eb44510126ddb56ae1a1d80b712b37ae55b3cf
Dervillay <james.gascoigneburns7@gmail.com>
2025-07-17 22:06:29 +0100

    Prevent flickering on page refresh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import ChatInterface from "./components/ChatInterface.tsx";
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoadingScreen } from "./components/LoadingScreen.tsx";
 import { PDFViewerModal } from "./components/PDFViewerModal.tsx";
 import { PDFViewerProvider } from "./contexts/PDFViewerContext.tsx";
+import { useColorMode, useToken } from "@chakra-ui/react";
 
 function App() {
 	const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+	const { colorMode } = useColorMode();
+
+	const [lightBackground, darkBackground] = useToken("colors", ["white", "#101010"]);
+
+	useEffect(() => {
+		document.body.style.background = colorMode === "dark" ? darkBackground : lightBackground;
+	}, [colorMode, darkBackground, lightBackground]);
 
 	if (isLoading) {
 		return <LoadingScreen />;
